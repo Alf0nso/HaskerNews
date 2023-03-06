5bfd4abfc62bd0eb350b4077e65f5ec60e49c301
Afonso Rafael <afonsorafael@sapo.pt>
2023-03-06 10:04:10 +0100

    Simple test case of scrapping hacker news.
    Using a simple selector get all the <tr> elements from the hacker news
    website.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,15 @@
 module Main (main) where
 
-import Lib
+import Text.HTML.Scalpel
 
 main :: IO ()
-main = someFunc
+main = do
+  something <- everything
+  case something of
+    Just s -> print s
+    nothing -> print "upssss"
+
+everything :: IO (Maybe [String])
+everything =
+  scrapeURL "https://news.ycombinator.com/" (htmls (tagSelector "tr"))
+
