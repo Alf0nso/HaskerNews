a8c4e099fae52753685281d8260f3b8b5bc2cb70
Afonso Rafael <afonsorafael@sapo.pt>
2023-03-06 16:06:59 +0100

    Get the rank number of the post.
    Instead of just returning the name of the post as string, return a
    tuple with the rank as a number and the name as string.

--- src/HackerNewsE.hs
+++ src/HackerNewsE.hs
@@ -10,7 +10,6 @@ module HackerNewsE ( allTag
 where
 
 import Text.HTML.Scalpel
-import Control.Applicative (many)
 
 hackerNewsUrl :: String
 hackerNewsUrl = "https://news.ycombinator.com/"
@@ -19,27 +18,38 @@ allTag :: Selector -> URL -> IO (Maybe [String])
 allTag s url =
   scrapeURL url (htmls s)
 
--- allPagePosts :: IO (Maybe [HNPost])
--- allPagePosts = scrapeURL hackerNewsUrl hackerNewsPosts
-
 data HNPost = HNPost { head :: String, subtext :: String }
 
+{- Post Title and respective rank. -}
 postHeader :: Selector
 postHeader = TagString "tr" @: [ hasClass "athing" ]
 
-hnPostHeader :: Scraper String [String]
-hnPostHeader = chroots postHeader
-               $ do title <- text $ TagString "span" @: [ hasClass
-                                                          "titleline" ]
-                    return title
+hnPostHeader :: Scraper String [(Int, String)]
+hnPostHeader =  chroots postHeader
+                $ do title <- text $ TagString "span" @: [ hasClass
+                                                           "titleline" ]
+                     rank  <- text $ TagString "span" @: [ hasClass
+                                                           "rank" ]
+                     
+                     return (rankToInt rank, title)
+  where
+    rankToInt :: String -> Int
+    rankToInt = read . init
 
-getPostTitle :: IO (Maybe [String])
+getPostTitle :: IO (Maybe [(Int, String)])
 getPostTitle = scrapeURL hackerNewsUrl hnPostHeader
-                      
-  
 
+
+{- Post number of comments and respective points. -}
 postSubText :: Selector
 postSubText = TagString "span" @: [ hasClass "subline" ]
+
+type Points         = Int
+type NumberComments = Int
+hnPostSubText :: Scraper String [(Int, Points)]
+hnPostSubText = 
+
+
 
 -- head :: Scraper String HNPost
 -- head = 
