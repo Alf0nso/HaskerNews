9599674216d4308bdb7c3783b5a83c178208ae98
Afonso Rafael <afonsorafael@sapo.pt>
2023-03-08 17:53:46 +0100

    Retrieved information converted into datatype and pretty printer.

--- src/HackerNewsE.hs
+++ src/HackerNewsE.hs
@@ -1,11 +1,10 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module HackerNewsE ( allTag
                    , hackerNewsUrl
                    , hnPostHeader
                    , getPostTitleRank
                    , getPostCommentPoint
-                   )
+                   , getInformationOn
+                   , printPosts )
 where
 
 import Text.HTML.Scalpel
@@ -13,9 +12,40 @@ import Data.List.Split ( splitOn )
 import Text.Read
 
 {- Helper functions -}
+type Title          = String
+type Points         = Int
+type NumberComments = Int
+type Rank           = Int
+data HackerNewsPost = HackerNewsPost { title    :: Title
+                                     , rank     :: Rank
+                                     , comments :: NumberComments
+                                     , points   :: Points }
+                      deriving Show
+
+{- print posts in a more readable way -}
+printPosts :: [HackerNewsPost] -> String
+printPosts [] = ""
+printPosts ((HackerNewsPost title rank comments points):posts) =
+  "Title:     " ++ title         ++ "\n" ++
+  "Rank:      " ++ show rank     ++ "\n" ++
+  "Points:    " ++ show points   ++ "\n" ++
+  "Comments:  " ++ show comments ++ "\n" ++
+  "----------------------------------\n" ++
+  printPosts posts
+
+combineAll :: [(Rank, Title)]
+           -> [(NumberComments, Points)]
+           -> [HackerNewsPost]
+combineAll [] [] = []
+combineAll ((rank,title):trs) ((comments, points):cps) =
+  (HackerNewsPost title rank comments points):combineAll trs cps
+
+{- The important url -}
 hackerNewsUrl :: String
 hackerNewsUrl = "https://news.ycombinator.com/"
 
+{- If false it's because it was not possible
+to convert into an integer -}
 getIntFromHNattr :: String -> String -> (Int, Bool)
 getIntFromHNattr s atr =
   case possibleInt of
@@ -41,23 +71,21 @@ hnPostHeader =  chroots postHeader
                                                            "rank" ]
                      return (fst $ getIntFromHNattr "." rank, title)
 
-getPostTitleRank :: IO (Maybe [(Int, String)])
+getPostTitleRank :: IO (Maybe [(Rank, Title)])
 getPostTitleRank = scrapeURL hackerNewsUrl hnPostHeader
 
 {- Post number of comments and respective points. -}
 postSubText :: Selector
 postSubText = TagString "span" @: [ hasClass "subline" ]
 
-type Points         = Int
-type NumberComments = Int
-
 hnPostSubText :: Scraper String [(NumberComments, Points)]
 hnPostSubText = chroots postSubText
-                $ do points   <- text $ TagString "span" @: [ hasClass
+                $ do points        <- text
+                                      $ TagString "span" @: [ hasClass
                                                               "score" ]
                      [_, comments] <- texts
-                                         $ TagString "a" @:
-                                         [ match checkCommentAttr ]
+                                      $ TagString "a" @:
+                                      [ match checkCommentAttr ]
 
                      return ( fst $ getIntFromHNattr "\160" comments
                             , fst $ getIntFromHNattr " " points)
@@ -68,3 +96,15 @@ hnPostSubText = chroots postSubText
 
 getPostCommentPoint :: IO (Maybe [(NumberComments, Points)])
 getPostCommentPoint = scrapeURL hackerNewsUrl hnPostSubText
+
+{- Combining all the information from both scrappings -}
+getInformationOn :: IO (Maybe [HackerNewsPost])
+getInformationOn = do ranksTitles    <- getPostTitleRank
+                      commentsPoints <- getPostCommentPoint
+
+                      case ranksTitles of
+                        Nothing -> return Nothing
+                        Just rt -> case commentsPoints of
+                                    Nothing -> return Nothing
+                                    Just cp ->
+                                      return $ Just $ combineAll rt cp
